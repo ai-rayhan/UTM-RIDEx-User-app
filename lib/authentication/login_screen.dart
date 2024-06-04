ac5c733a0bf1b83150b95fbb55be7e8eb515db7c
CmtBoy <alaminislam.dev@gmail.com>
2024-06-04 23:32:47 +0600

    login, register and splash design

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -8,195 +8,197 @@ import 'package:users_app/splashScreen/splash_screen.dart';
 import '../global/global.dart';
 import '../widgets/progress_dialog.dart';
 
-
-class LoginScreen extends StatefulWidget
-{
-
+class LoginScreen extends StatefulWidget {
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen>
-{
+class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailtextEditingController = TextEditingController();
   TextEditingController passwordtextEditingController = TextEditingController();
 
-  validateForm(){
-    if(!emailtextEditingController.text.contains("@"))
-    {
+  validateForm() {
+    if (!emailtextEditingController.text.contains("@")) {
       Fluttertoast.showToast(msg: "Email is not valid");
-    }
-    else if(passwordtextEditingController.text.isEmpty)
-    {
+    } else if (passwordtextEditingController.text.isEmpty) {
       Fluttertoast.showToast(msg: "Password is required");
-    }
-    else
-    {
+    } else {
       LoginUserNow();
     }
   }
 
-  LoginUserNow() async
-  {
+  LoginUserNow() async {
     showDialog(
-        context: context,
-        barrierDismissible: false, //dont want progress dialog disappear when user click outside
-        builder: (BuildContext c)
-        {
-          return ProgressDialog(message: "Processing, Please wait...",); //passing the sign up process
-        }
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext c) {
+        return ProgressDialog(message: "Processing, Please wait...");
+      },
     );
 
-    final User? firebaseUser = (
-        await fAuth.signInWithEmailAndPassword(
-          email: emailtextEditingController.text.trim(), //trim for space not counted
-          password: passwordtextEditingController.text.trim(), //if executed successfully, pass to firebase
-        ).catchError((msg){ //for error chance occur
-          Navigator.pop(context);
-          Fluttertoast.showToast(msg: "Error: " + msg.toString());
-        })
-    ).user;
+    final User? firebaseUser = (await fAuth
+            .signInWithEmailAndPassword(
+      email: emailtextEditingController.text.trim(),
+      password: passwordtextEditingController.text.trim(),
+    )
+            .catchError((msg) {
+      Navigator.pop(context);
+      Fluttertoast.showToast(msg: "Error: " + msg.toString());
+    }))
+        .user;
 
-    if(firebaseUser != null) //if user created successfully
-        { //recognize unique user id
-      DatabaseReference driversRef = FirebaseDatabase.instance.ref().child("users");
-      driversRef.child(firebaseUser.uid).once().then((driverKey){
+    if (firebaseUser != null) {
+      DatabaseReference driversRef =
+          FirebaseDatabase.instance.ref().child("users");
+      driversRef.child(firebaseUser.uid).once().then((driverKey) {
         final snap = driverKey.snapshot;
-        if(snap.value != null)
-        {
+        if (snap.value != null) {
           currentFirebaseUser = firebaseUser;
           Fluttertoast.showToast(msg: "Login Successful");
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen())); //splash screen have validation
-        }
-        else
-        {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (c) => const MySplashScreen()));
+        } else {
           Fluttertoast.showToast(msg: "No record exist with this email");
           fAuth.signOut();
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (c) => const MySplashScreen()));
         }
       });
-    }
-    else
-    {
-      Navigator.pop(context); // if not, display dialog msg
+    } else {
+      Navigator.pop(context);
       Fluttertoast.showToast(msg: "Login Failed");
-
     }
   }
 
+  bool showPass = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.grey[900],
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 30,),
-
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Image.asset("images/logo3.png"),
-              ),
-
-              const SizedBox(height: 10,),
-
-              const Text(
-                "Login as a User",
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.bold,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black, Colors.grey[900]!],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset("images/logo3.png", height: 100),
                 ),
-              ),
-
-              TextField(
-                controller: emailtextEditingController,
-                keyboardType: TextInputType.emailAddress,
-                style: const TextStyle(
-                    color: Colors.grey
-                ),
-                decoration: const InputDecoration(
-                  labelText: "Email",
-                  hintText: "Email",
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 10,
-                  ),
-                  labelStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14,
-                  ),
-
-                ),
-              ),
-
-              TextField(
-                controller: passwordtextEditingController,
-                keyboardType: TextInputType.text,
-                obscureText: true, //password text
-                style: const TextStyle(
-                    color: Colors.grey
-                ),
-                decoration: const InputDecoration(
-                  labelText: "Password",
-                  hintText: "Password",
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 10,
-                  ),
-                  labelStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14,
-                  ),
-
-                ),
-              ),
-
-              const SizedBox(height: 20,),
-
-              ElevatedButton(
-                onPressed: ()
-                {
-                  validateForm();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                ),
-                child: const Text(
-                  "Login",
+                const SizedBox(height: 20),
+                const Text(
+                  "Login as a User",
                   style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 18,
+                    fontSize: 26,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-
-              TextButton(
-                child: const Text(
-                  "Do not have an account? Register Here",
-                  style: TextStyle(color: Colors.grey),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: emailtextEditingController,
+                  keyboardType: TextInputType.emailAddress,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    labelText: "Email",
+                    hintText: "Email",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(color: Colors.white54),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    hintStyle: TextStyle(
+                      color: Colors.white54,
+                      fontSize: 14,
+                    ),
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
                 ),
-                onPressed: ()
-                {
-                  Navigator.push(context, MaterialPageRoute(builder: (c)=> SignUpScreen()));
-                },
-              ),
-
-            ],
+                const SizedBox(height: 10),
+                TextField(
+                  controller: passwordtextEditingController,
+                  keyboardType: TextInputType.text,
+                  obscureText: !showPass,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            showPass = !showPass;
+                          });
+                        },
+                        icon: Icon(showPass
+                            ? Icons.visibility_off
+                            : Icons.visibility)),
+                    isDense: true,
+                    labelText: "Password",
+                    hintText: "Password",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(color: Colors.white54),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    hintStyle: TextStyle(
+                      color: Colors.white54,
+                      fontSize: 14,
+                    ),
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    validateForm();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  ),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  child: const Text(
+                    "Do not have an account? Register Here",
+                    style: TextStyle(color: Colors.white54),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (c) => SignUpScreen()));
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -1,88 +1,81 @@
 import 'dart:async';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:users_app/assistants/assistant_methods.dart';
 import 'package:users_app/authentication/login_screen.dart';
 import 'package:users_app/mainScreen/main_screen.dart';
 
 import '../global/global.dart';
 
-
-class MySplashScreen extends StatefulWidget 
-{
+class MySplashScreen extends StatefulWidget {
   const MySplashScreen({Key? key}) : super(key: key);
 
   @override
   State<MySplashScreen> createState() => _MySplashScreenState();
 }
 
-
-
-class _MySplashScreenState extends State<MySplashScreen> 
-{
-  
-  startTimer()//how much time to display
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() //how much time to display
   {
-    fAuth.currentUser != null ? AssistantMethods.readCurrentOnlineUserInfo() : null;
+    fAuth.currentUser != null
+        ? AssistantMethods.readCurrentOnlineUserInfo()
+        : null;
 
-    Timer(const Duration(seconds: 3), () async
-    {
-      if(await fAuth.currentUser != null) //identify user is log in or not
-        {
-          currentFirebaseUser = fAuth.currentUser;
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> MainScreen()));
-        }
-      else
-        {
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> LoginScreen()));
-        }
-
+    Timer(const Duration(seconds: 3), () async {
+      if (await fAuth.currentUser != null) //identify user is log in or not
+      {
+        currentFirebaseUser = fAuth.currentUser;
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => MainScreen()));
+      } else {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => LoginScreen()));
+      }
     });
   }
-  
-  
+
   @override
-  void initState() { //called when go to any page
+  void initState() {
+    //called when go to any page
     super.initState(); //execute first/automatically
-    
+
     startTimer();
   }
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Material(
       child: Container(
-        // color: Colors.black,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-              Image.asset("images/logo3.png"),
-
-              const SizedBox(height: 10,),
-
-              const Text(
-                "Welcome to",
-                //"Ride Sharing Application",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
+          color: Colors.grey[900],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Spacer(
+                  flex: 1,
                 ),
-              ),
-              const Text(
-                "UTM Ridex",
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold
+                Padding(
+                  padding: const EdgeInsets.all(26.0),
+                  child: Lottie.asset("images/splash.json"),
                 ),
-              )
-            ],
-          ),
-        )
-      ),
+                Spacer(),
+                Text(
+                  "UTM Ridex",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.bold
+                      // fontFamily: 'Agne',
+                      ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
